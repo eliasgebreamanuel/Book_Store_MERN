898b214a71387bfffb6b85b2b22cb431dc5e33f4
eliasgebreamanuel <101189515+eliasgebreamanuel@users.noreply.github.com>
2023-09-30 07:31:04 +0300

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -2,4 +2,4 @@ export const PORT = 5555;
 
 
 export const mongoDBURL = 
-    'mongodb+srv://root:root@mern.xpsqbq0.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://root:<password>@mern.xpsqbq0.mongodb.net/?retryWrites=true&w=majority'
